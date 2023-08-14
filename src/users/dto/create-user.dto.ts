d6a4981f9471aed9411f9b2844c0d27a06443b49
Lucas Lins <lucas.fsl.92@gmail.com>
2023-08-13 22:13:28 -0300

    feature: adding ValidationPipe in the bootstrap function and create DTO for User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import { Contains, IsEmail, IsString, IsStrongPassword, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(100)
+    name: string;
+
+    @IsEmail()
+    @MaxLength(100)
+    email: string;
+
+    @IsStrongPassword({
+        minLength: 6, 
+        minNumbers: 1,
+        minLowercase: 1,
+        minSymbols: 1,
+        minUppercase: 1       
+    })
+    password: string;
+
+    
+
+
+}
